a448ceb830ab724a965d15a078a0373fdf02de55
Anthony Coutinho da Silva <thonycout@gmail.com>
2026-05-17 11:41:53 -0300

    feat(auth): session migration

---
+++ migrations/1779028826897_create-sessions-table.js
@@ -0,0 +1,52 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("sessions", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    user_id: {
+      type: "uuid",
+      notNull: true,
+      references: '"users"',
+      onDelete: "CASCADE",
+    },
+    token_hash: {
+      type: "bytea",
+      notNull: true,
+      unique: true,
+    },
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+    expires_at: {
+      type: "timestamptz",
+      notNull: true,
+    },
+    user_agent: {
+      type: "text",
+      notNull: false,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable("sessions");
+};
