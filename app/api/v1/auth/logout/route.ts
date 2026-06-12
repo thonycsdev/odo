45a3082f16cd424376af0dd3746e24d4dbe19d3f
Anthony Coutinho da Silva <thonycout@gmail.com>
2026-06-12 16:41:19 -0300

    fix(logout): set cookie to empty after logout

--- app/api/v1/auth/logout/route.ts
+++ app/api/v1/auth/logout/route.ts
@@ -5,7 +5,9 @@ import session from '@/models/session';
 export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
     await session.logoutUser(req);
-    return NextResponse.json({}, { status: 200 });
+    const response = NextResponse.json({}, { status: 200 });
+    response.cookies.delete('session_token');
+    return response;
   } catch (err) {
     const data = handle(err);
     return NextResponse.json(data, { status: data.status });
